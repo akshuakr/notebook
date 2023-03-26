78e1016ca5e16373aab040156bfbd4f4a9068ee7
akshu <in.akr.2000@gmail.com>
2023-03-27 00:48:01 +0530

    46

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?tls=false&readPreference=primary&directConnection=true";
+const mongoURI = "mongodb://localhost:27017/notebook?tls=false&readPreference=primary&directConnection=true";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, ()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require("express-validator");
 
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    // res.send('Hello ')
+router.post('/', 
 
-    const user = User(req.body);
-    user.save();
-
-
-    res.send(req.body);
+    body('name', 'Name should be at least 3 characters long.').isLength({min : 3}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password should be atleast 5 characters long.').isLength({min : 5}),
+    (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+    }).then((user) => res.json(user))
+    .catch(err=> console.log(err));
 })
 
 module.exports = router;
