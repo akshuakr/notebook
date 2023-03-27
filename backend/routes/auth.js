ecc34c9886318382e0c27464b4639cda117b3451
akshu <in.akr.2000@gmail.com>
2023-03-27 18:21:25 +0530

    50

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,44 @@ const jwt = require('jsonwebtoken');
 
 const JWT_SECRET = 'akshu';
 
+router.post('/login',
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password cannot be blank').exists(),
+
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {email, password} = req.body;
+
+        try{
+            let user = await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error: "Please enter correct credentials."});
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if(!passwordCompare){
+                return res.status(400).json({ error: "Please enter correct credentials." });
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, JWT_SECRET);
+            res.json({authToken});
+        }
+        catch(error){
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    }
+)
+
 // Create a user using: POST "api/auth/crateuser" . No Login Required
 router.post('/createuser', 
 
@@ -46,7 +84,7 @@ router.post('/createuser',
     } 
     catch(error){
         console.error(error.message);
-        res.status(500).send("Some Error Occured");
+        res.status(500).send("Internal Server Error");
     }
 
     
