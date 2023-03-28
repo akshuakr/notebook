7a053bc6466dd465d7ee7341b95a25a104d39992
akshu <in.akr.2000@gmail.com>
2023-03-28 11:48:42 +0530

    55

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ const port = 5000;
 app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 app.listen(port, ()=>{
     console.log(`Backend app is listening at https://localhost:${port}`);
