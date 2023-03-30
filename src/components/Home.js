1fb9425a9e59c46dd716b8852e0268666577c708
akshu <in.akr.2000@gmail.com>
2023-03-30 21:56:38 +0530

    61

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-import React, {useContext} from "react";
-import noteContext from "../context/notes/noteContext"
+import React from "react";
+import Notes from './Notes'
 
 const Home = () => {
     
-    const {notes, setNotes} = useContext(noteContext);
+    
     return (
         <>
             <div className="container my-3">
@@ -50,12 +50,9 @@ const Home = () => {
                     </button>
                 </form>
 
-                <div className="my-3">
-                    <h2>Your Notes</h2>
-                    {notes.map((note)=>{
-                        return note.title;
-                    })}
-                </div>
+                {/* </div> */}
+
+                <Notes/>
 
             </div>
         </>
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -25,12 +25,111 @@ const NoteState = (props) =>{
         {
           "_id": "6421d9a86bb8a16e6ac6c2ca",
           "user": "6421624a79372edc205281f3",
-          "title": "Title 4",
-          "description": "this is note 4.",
+          "title": "Title 3",
+          "description": "this is note 3.",
           "tag": "General",
           "date": "2023-03-27T18:00:08.924Z",
           "__v": 0
-        }
+        },
+        {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 4",
+            "description": "this is note 4.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 5",
+            "description": "this is note 5.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 6",
+            "description": "this is note 6.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 7",
+            "description": "this is note 7.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 8",
+            "description": "this is note 8.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 9",
+            "description": "this is note 9.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 10",
+            "description": "this is note 10.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 11",
+            "description": "this is note 11.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 12",
+            "description": "this is note 12.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 13",
+            "description": "this is note 13.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
+          {
+            "_id": "6421d9a86bb8a16e6ac6c2ca",
+            "user": "6421624a79372edc205281f3",
+            "title": "Title 14",
+            "description": "this is note 14.",
+            "tag": "General",
+            "date": "2023-03-27T18:00:08.924Z",
+            "__v": 0
+          },
       ]
 
       const [notes, setNotes] = useState(notesInitial)
